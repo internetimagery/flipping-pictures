5b132d540ad003465aee1c3f95d1878b3e1f3186
Jason <jason.dixon.email@gmail.com>
2015-04-07 04:58:06 +1200

    sdsf

--- testing/vimeo.js
+++ testing/vimeo.js
@@ -101,6 +101,7 @@ var buildSlider = function(parent, data){
 // START!
 jQuery(document).ready(function($) {
 	frameSize = 500; // Max size of the frame
+	alert("here");
 
 	// Set up vimeo player when ready
 	var playerID = $("#player")[0];
